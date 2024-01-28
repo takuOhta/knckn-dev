95c7f3e34c7053286c3260807d8045d3651ecb65
takuOhta <marotaku1302@gmail.com>
2024-01-28 20:26:22 +0900

    rename site

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,5 +1,5 @@
-export const SITE_URL = "";
-export const SITE_NAME = "";
-export const SITE_TITLE = "";
+export const SITE_URL = "https://astro-ogl.com";
+export const SITE_NAME = "astro-ogl";
+export const SITE_TITLE = "astro-ogl";
 export const SITE_KEYWORD = "";
 export const SITE_DESCRIPTION = "";
