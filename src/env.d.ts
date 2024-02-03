9a451c72a52569d741395017bdf2eba0bc5f4cf9
takuOhta <67897770+takuOhta@users.noreply.github.com>
2024-02-04 08:54:41 +0900

    add wordpress post page

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 interface ImportMetaEnv {
   readonly PUBLIC_SITE_PATH: string;
   readonly PUBLIC_SITE_NAME: string;
+  readonly PUBLIC_AAPI_ENDPOINT_WORDPRESS: string;
 }
 
 interface ImportMeta {
