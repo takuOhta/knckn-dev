61f1d90a64a70a48318dd3d680b8de717d22e37f
takuOhta <67897770+takuOhta@users.noreply.github.com>
2024-02-03 10:50:38 +0900

    rename site

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,5 +1,5 @@
 export const SITE_URL = "https://astro-ogl";
-export const SITE_NAME = "astro-ogl";
-export const SITE_TITLE = "astro-ogl";
+export const SITE_NAME = "knckn-dev";
+export const SITE_TITLE = "knckn-dev";
 export const SITE_KEYWORD = "";
-export const SITE_DESCRIPTION = "";
+export const SITE_DESCRIPTION = "knckn-dev";
