a503bd33f7fd1ef75ee774d2e8b34d8c0e149a91
takuOhta <67897770+takuOhta@users.noreply.github.com>
2024-06-30 17:06:26 +0900

    raycater追加

--- src/scripts/pages/webgl-school/003/FlyingPlane.ts
+++ src/scripts/pages/webgl-school/003/FlyingPlane.ts
@@ -10,6 +10,7 @@ import { MeshStandardMaterial } from 'three/src/materials/MeshStandardMaterial.j
 import { Vector2, Vector3 } from 'three'
 import { Raycaster } from 'three/src/core/Raycaster.js'
 import { globalMouseMove } from '@scripts/events/GlobalMouseEventHandler'
+import { Quaternion } from 'three/src/math/Quaternion.js'
 /**
  * IndexページのWEBGL Canvas
  */
@@ -18,12 +19,14 @@ class FlyingPlane extends BaseThreeCanvas {
   #directionalLight: DirectionalLight
   #ambientLight: AmbientLight
   #axesHelper: AxesHelper
-  #sphereMeshs: Array<Mesh> = []
+  sphereMeshs: Array<Mesh> = []
   planeMesh: Mesh | undefined = undefined
   currentTargetSphere: Mesh | undefined = undefined
-  targetDirection: Vector3 = new Vector3(0, 0, 0)
-  #planeVelocity: Vector3 = new Vector3(0, 0, 0)
+  previousTargetSphere: Mesh | undefined = undefined
+  targetPosition: Vector3 = new Vector3(0, 0, 0)
+  targetDirection: Vector3 = new Vector3(0, 1, 0).normalize()
   raycaster: Raycaster = new Raycaster()
+  isRaycasting = false
   mouse = new Vector2()
   constructor({ canvasElement }: { canvasElement: HTMLCanvasElement }) {
     super({ canvasElement, orbitControls: true })
@@ -46,7 +49,8 @@ class FlyingPlane extends BaseThreeCanvas {
   static SPHERE_SIZE = 100
   static SPHERE_LENGTH = 10
   static FAR = 3000
-  static PLANE_SPEED = 0.4
+  static PLANE_ROTATION_SPEED = 4.0
+  static PLANE_SPEED = 5.0
   /**
    * 初期化
    */
@@ -61,8 +65,22 @@ class FlyingPlane extends BaseThreeCanvas {
     this.planeMesh = this.#createPlaneMesh()
     this.scene.add(this.planeMesh)
     // 目標の惑星を設定
-    this.currentTargetSphere = this.#sphereMeshs[0]
+    this.currentTargetSphere = this.sphereMeshs[0]
+    this.previousTargetSphere = this.currentTargetSphere
+    this.targetPosition = this.currentTargetSphere.position.clone()
+    console.log(this.scene.children)
   }
+  /**
+   * createTargetSphereMesh
+   */
+  #createTargetSphereMesh() {
+    const geometry = new SphereGeometry(10)
+    const material = new MeshStandardMaterial({ color: 0xff0000 })
+    const mesh = new Mesh(geometry, material)
+    this.scene.add(mesh)
+    return mesh
+  }
+
   /**
    * createSphereMesh
    */
@@ -75,7 +93,9 @@ class FlyingPlane extends BaseThreeCanvas {
       const material = new MeshStandardMaterial({ color: 0x00ffff })
       const mesh = new Mesh(geometry, material)
       mesh.position.set(x, y, z)
-      this.#sphereMeshs.push(mesh)
+      const rand = 0.5 + Math.random() * 0.5
+      mesh.scale.set(rand, rand, rand)
+      this.sphereMeshs.push(mesh)
       this.scene.add(mesh)
     }
   }
@@ -86,6 +106,7 @@ class FlyingPlane extends BaseThreeCanvas {
     const geometry = new ConeGeometry(10, 40, 32)
     const material = new MeshStandardMaterial({ color: 0xb22222 })
     const mesh = new Mesh(geometry, material)
+    mesh.position.set(0, FlyingPlane.SPHERE_SIZE * 1.2, 0)
     return mesh
   }
   /**
@@ -93,41 +114,72 @@ class FlyingPlane extends BaseThreeCanvas {
    */
   override _update({ time }: { time: number }) {
     super._update({ time })
-    if (!this.planeMesh || !this.targetDirection || !this.currentTargetSphere) return
+    if (!this.planeMesh || !this.targetDirection || !this.currentTargetSphere || !this.previousTargetSphere) return
+
+    // 現在の方向を取得
+    const previousPosition = this.planeMesh.position.clone()
+    const previoustDirection = this.targetDirection.clone()
+
     // // マウスと重なったオブジェクトを取得
-    // this.raycaster.setFromCamera(this.mouse, this._camera)
-    // const intersects = this.raycaster.intersectObjects(this.scene.children)
-    // if (intersects.length > 0) {
-    //   this.currentTargetSphere.position.set(intersects[0].point.x, intersects[0].point.y, intersects[0].point.z)
-    // }
-    // 飛行機の目標方向を設定
+    this.raycaster.setFromCamera(this.mouse, this._camera)
+    const intersects = this.raycaster.intersectObjects(this.sphereMeshs)
+    if (intersects.length > 0 && this.currentTargetSphere.uuid != intersects[0].object.uuid) {
+      this.isRaycasting = true
+    }
+
+    // // 飛行機の目標方向を設定
     // this.currentTargetSphereを中心とした半径SPHERE_SIZE＊1.2の球上の位置を取得
-    const cx = this.currentTargetSphere.position.x
-    const cy = this.currentTargetSphere.position.y
-    const cz = this.currentTargetSphere.position.z
-    const angle = new Vector2(0.433 * time, 0.233 * time)
-    const radius = FlyingPlane.SPHERE_SIZE * 1.4
-    const x = cx + radius * Math.sin(angle.x) * Math.cos(angle.y)
-    const y = cy + radius * Math.sin(angle.x) * Math.sin(angle.y)
-    const z = cz + radius * Math.cos(angle.x)
-    this.targetDirection.set(x, y, z)
-    // 飛行機の方向と速度を設定
-    this.#planeVelocity.subVectors(this.targetDirection, this.planeMesh.position)
-    this.#planeVelocity.multiplyScalar(FlyingPlane.PLANE_SPEED)
-    this.planeMesh.position.add(this.#planeVelocity)
-    // this.planeMesh.lookAt(this.targetDirection)
+    const cx = (this.currentTargetSphere.position.x - this.targetPosition.x) * 0.07
+    const cy = (this.currentTargetSphere.position.y - this.targetPosition.y) * 0.07
+    const cz = (this.currentTargetSphere.position.z - this.targetPosition.z) * 0.07
+    this.targetPosition.add(new Vector3(cx, cy, cz))
+
+    // cameraのlookAtを設定
+    this._camera.lookAt(this.targetPosition.x, this.targetPosition.y, this.targetPosition.z)
+    // 飛行機の位置を設定
+    const angle = new Vector2(0.433 * time, 0.233 * time).multiplyScalar(FlyingPlane.PLANE_SPEED)
+    const radius = FlyingPlane.SPHERE_SIZE * this.currentTargetSphere.scale.x * 1.2
+    const x = this.targetPosition.x + radius * Math.sin(angle.x) * Math.cos(angle.y)
+    const y = this.targetPosition.y + radius * Math.sin(angle.x) * Math.sin(angle.y)
+    const z = this.targetPosition.z + radius * Math.cos(angle.x)
+    // 一を設定
+    this.planeMesh.position.set(x, y, z)
+
+    // 前フレームとの位置と進行方向の差分ベクトルを取得
+    const subPositionVector = new Vector3().subVectors(this.planeMesh.position, previousPosition)
+    subPositionVector.normalize()
+    // 進行方向ベクトりに無機ベクトルをスケールして加算
+    this.targetDirection.add(subPositionVector.multiplyScalar(1.0))
+    this.targetDirection.normalize()
+
+    // // 飛行機の回転
+    const normalAxis = new Vector3().crossVectors(previoustDirection, this.targetDirection)
+    normalAxis.normalize()
+    const cos = previoustDirection.dot(this.targetDirection)
+    const radian = Math.acos(cos)
+    const qtn = new Quaternion().setFromAxisAngle(normalAxis, radian)
+    this.planeMesh.quaternion.premultiply(qtn)
   }
   /**
    * addEventListeners
    */
   #addEventListeners() {
     const boundOnMouseMove = this.#onMouseMove.bind(this)
+    const boundOnMouseUp = this.#onMouseUp.bind(this)
     globalMouseMove.add(boundOnMouseMove)
+    document.addEventListener('mouseup', boundOnMouseUp, false)
   }
 
   #onMouseMove = (event: MouseEvent) => {
     this.mouse.x = (event.clientX / this.width) * 2 - 1
     this.mouse.y = -((event.clientY / this.height) * 2 - 1)
+  }
+  #onMouseUp = () => {
+    console.log('click')
+    if (this.isRaycasting) {
+      this.previousTargetSphere = this.currentTargetSphere
+      this.currentTargetSphere = this.raycaster.intersectObjects(this.sphereMeshs)[0].object as Mesh
+    }
   }
 
   /**
